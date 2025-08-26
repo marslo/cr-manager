6054c2a8de2202063d23ae037ee813c9c4272acb
marslo <marslo@marvell.com>
2025-08-25 22:36:55 -0700

    test: add test resources into single branch
    Signed-off-by: marslo <marslo@marvell.com>

---
+++ sample.c
@@ -0,0 +1,8 @@
+/**
+ * Copyright © 2024 marslo
+ * Licensed under the MIT License, Version 1.0
+ *
+ * @author: marslo
+ * @date: 2025-01-01
+ * @description: Sample C code for demonstration purposes.
+ */
