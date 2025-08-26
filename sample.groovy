6f08d2f1f2ff3d6975d124137cf4a72d41011773
marslo <marslo.jiao@gmail.com>
2025-08-25 22:36:55 -0700

    test: add test resources into single branch
    Signed-off-by: marslo <marslo@marvell.com>

---
+++ sample.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+/**
+ *******************************************************************************
+ * Copyright © 2024 [Your Company]                                             *
+ * Licensed under the Apache License, Version 2.0                              *
+ *******************************************************************************
+ *
+ * @author       marslo@mail.com
+ * @since        2025-01-07
+ * @description  this class is for message constants, including job description, css style, etc.
+**/
+
+import groovy.transform.Field
+
+// vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=groovy
