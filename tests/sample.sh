73c58fce4b168cb3a2244fc14dcb939100376d2d
marslo <marslo.jiao@gmail.com>
2026-05-06 19:19:49 -0700

    fix(run.sh): fix the `run.sh` to handle if the script is sourced or executed, and handle exit/return properly. (#29)
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- tests/sample.sh
+++ tests/sample.sh
@@ -3,12 +3,8 @@
 set -euo pipefail
 
 # ============================================================================ #
-# Copyright (c) 2026 Marvell. All rights reserved.                             #
-# The following file is subject to the limited use license agreement by and    #
-# between Marvell and you, your employer or other entity on behalf of whom     #
-# you act. In the absence of such license agreement the following file is      #
-# subject to Marvell's standard Limited Use License Agreement.                 #
-# https://www.marvell.com/agreement.html                                       #
+# Copyright © 2024 NAME HERE                                                   #
+# Licensed under the MIT License, Version 1.0                                  #
 # ============================================================================ #
 #
 # @author       marslo
